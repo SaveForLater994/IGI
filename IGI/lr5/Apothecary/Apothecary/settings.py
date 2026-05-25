08851a2cdb4cd1869042a36fce3d29c9851aa27f
SaveForLater <aliaksandrkushniarov@gmail.com>
2026-05-25 08:35:17 +0300

    2nd

--- IGI/lr5/Apothecary/Apothecary/settings.py
+++ IGI/lr5/Apothecary/Apothecary/settings.py
@@ -91,7 +91,12 @@ else:
             'PORT': '5432',
         }
     }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
